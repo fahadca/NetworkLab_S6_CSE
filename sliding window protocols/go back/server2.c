2a54c5d35c5a304c95d294e2aa21d86561cf62e8
Fahad Abdullah <102849603+fahadca@users.noreply.github.com>
2024-07-04 09:09:03 +0530

    Update server2.c

--- sliding window protocols/go back/server2.c
+++ sliding window protocols/go back/server2.c
@@ -6,90 +6,89 @@
 #include<netinet/in.h>
 #include<unistd.h>
 
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
+#define BUFFER_SIZE 80
+#define SERVER_PORT 8080
+#define SOCKET_ADDRESS struct sockaddr
 
-void sendMessage(int sockfd, int ack) {
-    char buff[MAX];
-    sprintf(buff, "%d", ack);
-    send(sockfd, buff, strlen(buff), 0);
+void sendAcknowledgement(int socketDescriptor, int acknowledgement) {
+    char buffer[BUFFER_SIZE];
+    sprintf(buffer, "%d", acknowledgement);
+    send(socketDescriptor, buffer, strlen(buffer), 0);
 }
 
 int main() {
-    int sockfd, connfd, len;
-    struct sockaddr_in serveraddr, client;
+    int socketDescriptor, connectionDescriptor, clientLength;
+    struct sockaddr_in serverAddress, clientAddress;
 
     // Create socket
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socket creation failed\n");
+    if((socketDescriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation failed\n");
         exit(0);
     }
     else
-        printf("socket created successfully\n");
+        printf("Socket created successfully\n");
 
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    serveraddr.sin_port = htons(PORT);
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(SERVER_PORT);
 
-    if (bind(sockfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0) {
+    if (bind(socketDescriptor, (SOCKET_ADDRESS *) &serverAddress, sizeof(serverAddress)) < 0) {
         perror("Bind failed. Error");
         return 1;
     }
     printf("Bind done\n");
 
-    listen(sockfd, 5);
-    printf("server listening .. \n");
+    listen(socketDescriptor, 5);
+    printf("Server listening .. \n");
 
-    len = sizeof(client);
-    connfd = accept(sockfd, (SA *) &client, &len);
+    clientLength = sizeof(clientAddress);
+    connectionDescriptor = accept(socketDescriptor, (SOCKET_ADDRESS *) &clientAddress, &clientLength);
 
-    char buff[MAX];
-    int f, c, ack, next = 0;
+    char buffer[BUFFER_SIZE];
+    int frameNumber, randomChoice, acknowledgement, expectedFrame = 0;
 
     while(1) {
         sleep(1);
-        memset(buff, 0, MAX);
-        recv(connfd, buff, MAX, 0);
+        memset(buffer, 0, BUFFER_SIZE);
+        recv(connectionDescriptor, buffer, BUFFER_SIZE, 0);
 
-        if(strcmp("EXIT", buff) == 0) {
-            printf("exit\n");
-           // break;
-           exit(0);
+        if(strcmp("EXIT", buffer) == 0) {
+            printf("Exiting\n");
+            exit(0);
         }
 
-        f = atoi(buff);
+        frameNumber = atoi(buffer);
 
-        if(f != next) {
-            printf("Frame %d discarded\n acknoledgement sent:%d\n", f, ack);
-            sendMessage(connfd, ack);
+        if(frameNumber != expectedFrame) {
+            printf("Frame %d discarded\nAcknowledgement sent: %d\n", frameNumber, acknowledgement);
+            sendAcknowledgement(connectionDescriptor, acknowledgement);
             continue;
         }
 
-        c = rand() % 3;
+        randomChoice = rand() % 3;
 
-        switch(c) {
+        switch(randomChoice) {
             case 0:
-                printf("Frame %d not received\n", f);
+                printf("Frame %d not received\n", frameNumber);
                 break;
             case 1:
-                ack = f;
+                acknowledgement = frameNumber;
                 sleep(2);
-                printf("Frame %d received\nAcknowledgement sent:%d\n", f, ack);
-                sendMessage(connfd, ack);
-                next = ack + 1;
+                printf("Frame %d received\nAcknowledgement sent: %d\n", frameNumber, acknowledgement);
+                sendAcknowledgement(connectionDescriptor, acknowledgement);
+                expectedFrame = acknowledgement + 1;
                 break;
             case 2:
-                ack = f;
-                printf("Frame %d received\nAcknowledgement sent:%d\n", f, ack);
-                sendMessage(connfd, ack);
-                next = ack + 1;
+                acknowledgement = frameNumber;
+                printf("Frame %d received\nAcknowledgement sent: %d\n", frameNumber, acknowledgement);
+                sendAcknowledgement(connectionDescriptor, acknowledgement);
+                expectedFrame = acknowledgement + 1;
                 break;
         }
     }
 
-    close(sockfd);
+    close(socketDescriptor);
 
     return 0;
 }
